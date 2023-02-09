4ca04b0043218d38ddc7fd4e8f2f0bd013cb4be5
Notgard <raoul.behaghie@gmail.com>
2023-02-09 16:01:37 +0100

    Minor updates, binary trees to refine later
    Need to manage terminal nodes for binary trees and a basic print for easier visualisation

--- BinaryTree.py
+++ BinaryTree.py
@@ -25,6 +25,7 @@ class BinaryTree:
 
     def add(self, data: Tuple):
         if self.left == None:
+            print("left", data)
             self.left = BinaryTree()
             self.left.data = data
             self.data += data[1]
@@ -32,11 +33,19 @@ class BinaryTree:
             self.right = BinaryTree()
             self.right.data = data
             self.data += data[1]
-        else:
+        elif self.data < data[1]:
             self.left = BinaryTree(self)
             self.right = None
             self.add(data)
+        else: #if tree is full, create new tree: right branch with the old tree and the left branch with the value
+            print("ok")
+            self.right = BinaryTree(self)
+            print(self.right.data)
+            self.left = None
+            self.add(data)
 
+    def __str__(self):
+        pass
 
 if __name__ == "__main__":
     tree = BinaryTree()
@@ -57,3 +66,11 @@ if __name__ == "__main__":
     for value in values:
         tree.add(value)
     print(list(tree))
+
+    tree = BinaryTree()
+    values = [("a", 3), ("b", 3), ("c", 1)]
+    values.sort(key=lambda x: x[1])
+    print(values)
+    for value in values:
+        tree.add(value)
+    print(tree.left.data, tree.data, tree.right)
--- CompresseurHuffman.py
+++ CompresseurHuffman.py
@@ -1,6 +1,7 @@
 from Binaire603 import *
 import BinaryTree
 
+
 class CompresseurHuffman(object):
     def __init__(self) -> None:
         pass
@@ -9,11 +10,12 @@ class CompresseurHuffman(object):
         assert isinstance(monBin, Binaire603), "monBin n'est pas un Binaire603"
         lf = monBin.lFrequences()
         lbin = sorted(list(set([strbin for strbin in monBin.toString()])))
-        lf = list(filter(lambda x: x!=0, lf))
+        lf = list(filter(lambda x: x != 0, lf))
         return dict(zip(lbin, lf))
 
     def arbreDepuisListe(lp):
         pass
+
 
 if __name__ == "__main__":
     comp = CompresseurHuffman()
